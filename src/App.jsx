95196e630e1f9588d6b4d7ecf77a7224ece0a797
Vladyslav Salenik <vladyslav.salenik@gmail.com>
2024-04-15 02:35:13 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import { nanoid } from "nanoid";
 import "./App.css";
-import localFunc from "./localFunc";
 
 function App() {
-  const fetchContacts = [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ];
-
-  const [contacts, setContacts] = useState(() => {
-    const localContacts = localFunc.load("contacts");
-
-    if (localContacts !== undefined) {
-      return localContacts;
-    }
-
-    return fetchContacts;
-  });
-
-  useEffect(() => {
-    localFunc.save("contacts", contacts);
-  }, [contacts]);
-
   return (
     <div className="container">
       <h1>Phonebook</h1>
